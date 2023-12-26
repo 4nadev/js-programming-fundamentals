4aaed2f5993bfffc904a53dedda0c3d86239c904
Aninha <anamarianevesbatista@gmail.com>
2023-12-26 15:58:31 -0300

    feat: 🎸 New project

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,15 @@
+
+alert('Boas vindas ao App, vamos jogar?');
+
+let name = prompt('Qual o seu nome ?');
+
+let tentativa = prompt('Escolha um numero entre 1 e 33');
+
+let numberSecret = '33';
+
+if (numberSecret == tentativa ) {
+
+    alert('Você acertou');
+} else {
+    alert('Você errou')
+}
