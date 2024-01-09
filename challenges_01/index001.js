fa412d131b89f931caa71c60d63ebc7d3d87f77c
Aninha <anamarianevesbatista@gmail.com>
2024-01-09 14:17:12 -0300

    feat: 🎸 Novos desafios

---
+++ challenges_01/index001.js
@@ -0,0 +1,31 @@
+// Create a program that uses console.log to display a welcome message.
+console.log('Hello World')
+
+//Create a variable called "name" and give it your name. Then use console.log to display the message "Hello, [your name]!" in the browser console.
+let name = 'Ana Maria';
+console.log(`Olá, ${name}`);
+
+//Create a variable called "name" and give it your name. Then use the alert to display the message "Hello, [your name]!" .
+let nome = 'Ana Maria';
+// alert(`Olá, ${nome}`);
+
+//Use the prompt and ask the following question: What programming language do you like most? Then store the response in a variable and show it in the browser console.
+// var langLike = prompt('What programming language do you like most?');
+// console.log(langLike);
+
+//Create a variable called "value1" and another called "value2", assigning them numeric values ​​of your choice. Then, add these two values ​​and store the result in a third variable called "result". Use console.log to display the message "The sum of [value1] and [value2] is equal to [result]." in the console.
+ let value1 = 2
+ let value2 = 3
+ let  result = value1 + value2 ;
+ console.log(`A soma de  ${value1} e  ${value2} é igual a  ${result}`);
+
+//Ask user to enter their age with prompt. Based on the age entered, use an if to check whether the person is older or younger, displaying an appropriate message in the console.
+// let age = prompt('insira sua idade');
+// if(age <= 18 ){
+//     console.log('Você é menor de idade');
+// } else {
+//     console.log('Você é maior de idade');
+// }
+
+//Create a variable "number" and ask for a value with prompt check if it is positive, negative or zero. Use if-else to print the respective message.
+
